c8c8e40693cf48d89740056291bae97f21834cc8
Daniel Jérusalmy <djerusal@uqac.ca>
2024-07-14 15:20:22 -0400

    adjust header text, add useEffect to handle dark mode on render

--- app/[lang]/(home)/components/header.tsx
+++ app/[lang]/(home)/components/header.tsx
@@ -10,8 +10,8 @@ type Props = {
 
 export const Header = ({ data }: Props) => {
   return (
-    <div className="flex flex-col items-center justify-around lg:justify-between min-h-[80vh]">
-      <div className="flex flex-col lg:flex-row max-w-screen-xl w-full gap-16 items-center justify-center mt-12">
+    <div className="flex flex-col items-center justify-center lg:justify-between min-h-[80vh]">
+      <div className="flex flex-col max-w-screen-xl w-full gap-16 items-center justify-center mt-12">
         <div className="max-w-[250px] max-h-[250px] md:max-w-[350px] md:max-h-[350px] xl:max-w-[450px] xl:max-h-[450px] animate-in">
           <Image
             src="https://contentstatic.techgig.com/photo/76220523/4-secrets-to-become-a-professional-programmer.jpg?140622"
@@ -21,10 +21,10 @@ export const Header = ({ data }: Props) => {
             className="rounded-full"
           />
         </div>
-        <div className="flex-1 flex flex-col gap-4 items-start justify-center animate-in delay-100">
-          <p className="tracking-widest leading-[1.8] px-8 lg:px-1 lg:max-w-[85%] md:text-sm/loose">
+        <div className="flex-1 flex flex-col gap-4 items-center justify-center animate-in delay-100">
+          <p className="tracking-wide leading-[1.7] px-8 lg:px-0 lg:max-w-[75%] text-sm/loose lg:text-lg/loose text-pretty text-center ">
             {data.summary1}{" "}
-            <span className="text-green-600 dark:text-green-400">
+            <span className="text-green-500 font-bold dark:text-green-400">
               {data.colorText}
             </span>
             {data.summary2}
--- app/[lang]/(home)/components/logo.tsx
+++ app/[lang]/(home)/components/logo.tsx
@@ -3,13 +3,19 @@ import { useTheme } from "next-themes";
 import Image from "next/image";
 import dark from "@/assets/images/dj-dark.png";
 import light from "@/assets/images/dj-light.png";
+import { useEffect, useState } from "react";
 type Props = {};
 
 export const Logo = (props: Props) => {
   const { theme } = useTheme();
+  const [currentTheme, setCurrentTheme] = useState("light");
+  useEffect(() => {
+    if (theme == "light") setCurrentTheme("light");
+    else setCurrentTheme("dark");
+  }, [theme]);
   return (
     <Image
-      src={theme != "dark" ? dark : light}
+      src={currentTheme != "dark" ? dark : light}
       alt=""
       width={300}
       height={300}
